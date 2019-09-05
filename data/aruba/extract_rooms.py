44f719e26dbb5419304b69a234756139a5b5147b
Jiri Janota <janotjir@fel.cvut.cz>
2019-09-05 12:04:15 +0200

    Prepared dataset for experiment - generated presence files

--- data/aruba/extract_rooms.py
+++ data/aruba/extract_rooms.py
@@ -9,36 +9,40 @@ import abc
 #       or incoming (into the room, so person is there from now on until leaving)
 #       (all regarding M sensors not D)
 aruba_topological_location_Msensor_mapping = {
-    "kitchen":          [14, 15, 16, 17, 18, 19],
-    "living_room":      [9, 10, 12, 13, 20],
-    "apartment_center": [8, 11, 21],
-    "master_bedroom":   [1, 2, 3, 5, 7],
-    "master_bathroom":  [4],
-    "corridor":         [22, 30],
-    "second_bathroom":  [31],
-    "office":           [25, 26, 27],
-    "second_bedroom":   [24]
+    "kitchen":          ["M014", "M019"], #"M015", "M016", "M017", "M018",
+    "living_room":      ["M020"], #"M009", "M010", "M012", "M013",
+    "apartment_center": ["M008", "M011", "M021"],
+    "master_bedroom":   ["M007"], #"M001", "M002", "M003", "M005", 
+    "master_bathroom":  ["M004"],
+    "corridor":         ["M022", "M030"],
+    "second_bathroom":  ["M031", "D003"],
+    "office":           ["M027"], #"M025", "M026", 
+    "second_bedroom":   ["M024"],
+    "outside":          ["DOO1", "D002", "D004"]
 }
 
 locations_number = len(aruba_topological_location_Msensor_mapping)
 # map locations to their idx (number)
 locations_number_map = dict([(aruba_topological_location_Msensor_mapping.keys()[i], i) for i in range(locations_number)])
 
+print(locations_number_map)
+
+
 # cache of inverse mapping to aruba_topological_location_Msensor_mapping
 sensor_location_map = dict()
 for k, v in aruba_topological_location_Msensor_mapping.iteritems():
     for el in v:
         sensor_location_map[el] = k
-
+        
 
 def get_timestamp_from_datetime(dt, unix=True):
     if unix:  # Unix timestamp (since 1970-01-01)
         reference = datetime.datetime(1970, 1, 1)
     else:  # Since time 0
-        reference = (dt - datetime.datetime.min).total_seconds()
+        reference = datetime.datetime.min
     return (dt - reference).total_seconds()
-
-
+    
+    
 class SensoryData:
     def __init__(self, date, time, sensor_name, data):
         date = date.split('-')
@@ -49,8 +53,9 @@ class SensoryData:
         self.sensor_type = sensor_name[0]
         if self.sensor_type != 'M' and self.sensor_type != 'T' and self.sensor_type != 'D':
             raise ValueError("Invalid sensor")
-        self.sensor_number = int(sensor_name[1:])
-        self.sensor_data = data.replace("c", '').replace("5", '')
+        self.sensor_name = sensor_name
+        #self.sensor_number = int(sensor_name[1:])
+        self.sensor_data = data.replace("c", '').replace("5", '') #???
 
     @staticmethod
     def from_string(string):
@@ -59,7 +64,7 @@ class SensoryData:
         string = string.split(' ')
         string = filter(lambda x: x != '', string)
         return SensoryData(string[0], string[1], string[2], string[3])
-
+	
 
 if __name__ == "__main__":
     data_file = sys.argv[1]
@@ -74,27 +79,27 @@ if __name__ == "__main__":
         # first_time = datetime.datetime(year=d.year, month=d.month)
         # first_time += datetime.timedelta(days=1)
 
-        i = 0
+        #i = 0
         for line in f:
-            i += 1
-            if i > 1000:
-                break
+            #i += 1
+            #if i > 1000:
+                #break
             try:
                 d = SensoryData.from_string(line)
             except ValueError:
                 continue
 
-            # filter out non-motion sensors
-            if d.sensor_type != 'M':
+            # filter out non-motion and non-door sensors
+            if d.sensor_type != 'M' and d.sensor_type != 'D':
                 continue
 
             # filter out not included sensors
-            if d.sensor_number not in sensor_location_map:
+            if d.sensor_name not in sensor_location_map:
                 continue
 
             # filter out turn-off events - they are irrelevant to our analysis
-            # if d.sensor_data != 'ON':
-            #     continue
+            if d.sensor_data != 'ON' and d.sensor_data != 'OPEN':
+                continue
 
             s_data.append(d)
             if d.datetime > largest_time:
@@ -103,43 +108,49 @@ if __name__ == "__main__":
                 smallest_time = d.datetime
 
 
-    largest_time = largest_time.replace(second=0, microsecond=0) + datetime.timedelta(minutes=1)
-    smallest_time = smallest_time.replace(second=0, microsecond=0)
-    smallest_time_timestamp = get_timestamp_from_datetime(smallest_time)
-
     # in seconds, number of seconds per slot
-    resolution = 0.5
-
-    # get number of timeslots, for which there are data
-    range_of_timeslots = int((largest_time - smallest_time).total_seconds() / resolution + 1)
-    print range_of_timeslots
-    result = [[0 for _ in range(locations_number)] for _ in range(range_of_timeslots)]
-
-    for d in s_data:
-        timestamp = get_timestamp_from_datetime(d.datetime.replace(second=0, microsecond=0))
-        location = sensor_location_map[d.sensor_number]
-        idx = int((timestamp - smallest_time_timestamp)/resolution)
-        if result[idx][locations_number_map[location]] == 1:
-            continue
-        elif d.sensor_data == 'ON':
-            result[idx][locations_number_map[location]] = 1
-        else:
-            result[idx][locations_number_map[location]] = -1
-
-    for r in result:
-        print r
-
-    state = result[0]
-    for i in range(len(result)):
-        state = [(lambda x: 0 if x == -1 else (1 if x == 1 else state[j]))(result[i][j]) for j in range(locations_number)]
-        result[i] = state
-
-    # for r in result:
-    #     print r
+    resolution = 60
+    
+    # align times according to resolution
+    largest_time_timestamp = get_timestamp_from_datetime(largest_time)
+    largest_time_timestamp = (largest_time_timestamp - (largest_time_timestamp % resolution)) + resolution
+    largest_time = datetime.datetime.utcfromtimestamp(largest_time_timestamp)
 
 
-
-
-
-
-
+    smallest_time_timestamp = get_timestamp_from_datetime(smallest_time)
+    smallest_time_timestamp = (smallest_time_timestamp - (smallest_time_timestamp % resolution)) + resolution
+    smallest_time = datetime.datetime.utcfromtimestamp(smallest_time_timestamp)
+        
+    # extract the rooms, output = date, time, active room
+    tf = open(target_file, 'w')
+    s_id = 0
+    d = s_data[s_id] # get first sensor
+    timestamp = get_timestamp_from_datetime(d.datetime.replace(microsecond = 0)) # get time when was sensor ON
+    door = False
+    tmp_d = None
+    while(smallest_time_timestamp < largest_time_timestamp): 
+    	while(timestamp < smallest_time_timestamp): # searching for the last sensor that was ON
+    		if(not door):
+    			s_id += 1
+    			d = s_data[s_id]
+    			timestamp = get_timestamp_from_datetime(d.datetime.replace(microsecond = 0))
+    			if(d.sensor_type == 'D'):
+    				door = True
+    				tmp_d = d
+    		else:
+    			s_id += 1
+    			d = s_data[s_id]
+    			timestamp = get_timestamp_from_datetime(d.datetime.replace(microsecond = 0))
+    			
+    	if(door):
+    		s_data[s_id - 1] = tmp_d
+    		door = False		
+    			
+    	location = sensor_location_map[s_data[s_id - 1].sensor_name]
+    	
+    	strtime =  (datetime.datetime.utcfromtimestamp(smallest_time_timestamp)).strftime("%Y-%m-%d %H:%M:%S")
+    	tf.write(strtime + " " + location + "\n")
+    
+    	smallest_time_timestamp += resolution
+    
+    tf.close
---
+++ data/aruba/rooms_to_files.py
@@ -0,0 +1,75 @@
+import sys
+import datetime
+
+# program takes extracted_rooms as an argument and create text files for every room and mark presence of person
+
+# 0 outside
+# 1 apartment_center
+# 2 master_bathroom
+# 3 office
+# 4 corridor
+# 5 second_bathroom
+# 6 second_bedroom
+# 7 living_room
+# 8 master_bedroom
+# 9 kitchen
+ 
+
+def get_timestamp_from_datetime(dt, unix=True):
+    if unix:  # Unix timestamp (since 1970-01-01)
+        reference = datetime.datetime(1970, 1, 1)
+    else:  # Since time 0
+        reference = datetime.datetime.min
+    return (dt - reference).total_seconds()
+
+def any_room_to_file(room_f, goal):
+	data_f = open(sys.argv[1], "r")
+	for line in data_f:
+		line = line.split(" ")
+		date = (line[0]).split("-")
+		time = (line[1]).split(":")
+		location = line[2]
+		location = location[:len(location)-1]
+		
+		timestamp = datetime.datetime(int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1]), int(time[2]))
+		timestamp = get_timestamp_from_datetime(timestamp)
+		
+		room_f.write(str(int(timestamp)))
+		if location == goal:
+			room_f.write(" 1\n")
+		else:
+			room_f.write(" 0\n")
+	#print("DONE")
+	data_f.close()
+	room_f.close()
+	
+
+outside_f = open("presence_0.txt", "w")
+any_room_to_file(outside_f, "outside")
+
+apartment_center_f = open("presence_1.txt", "w")
+any_room_to_file(apartment_center_f, "apartment_center")
+
+master_bathroom_f = open("presence_2.txt", "w")
+any_room_to_file(master_bathroom_f, "master_bathroom")
+
+office_f = open("presence_3.txt", "w")
+any_room_to_file(office_f, "office")
+
+corridor_f = open("presence_4.txt", "w")
+any_room_to_file(corridor_f, "corridor")
+
+second_bathroom_f = open("presence_5.txt", "w")
+any_room_to_file(second_bathroom_f,"second_bathroom")
+
+second_bedroom_f = open("presence_6.txt", "w")
+any_room_to_file(second_bedroom_f, "second_bedroom")
+
+living_room_f = open("presence_7.txt", "w")
+any_room_to_file(living_room_f, "living_room")
+
+master_bedroom_f = open("presence_8.txt", "w")
+any_room_to_file(master_bedroom_f, "master_bedroom")
+
+kitchen_f = open("presence_9.txt", "w")
+any_room_to_file(kitchen_f, "kitchen")
